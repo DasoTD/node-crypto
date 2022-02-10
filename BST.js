815b30ebcfef6956c9816787f6dde09e693cefa3
Daso <daso.timileyin@gmail.com>
2022-02-10 15:17:34 +0100

    BST

--- BST.js
+++ BST.js
@@ -11,22 +11,22 @@ class BinarySearchTree {
          this.root = null;
      }
 
-//      find(value){
-//          if(this.root === null) return false;
-//          var current = this.root;
-//          found = false;
-//          while(current && !found){
-//              if(value < current.value){
-//                  current = current.left;
-//              } else if(current > current.value){
-//                  current = current.right;
-//              } else {
-//                  found = true;
-//              }
-//          }
-//          if(!found) return undefined;
-//          return current;
-//      }
+     find(value){
+         if(this.root === null) return false;
+         var current = this.root;
+         found = false;
+         while(current && !found){
+             if(value < current.value){
+                 current = current.left;
+             } else if(current > current.value){
+                 current = current.right;
+             } else {
+                 found = true;
+             }
+         }
+         if(!found) return undefined;
+         return current;
+     }
 
 contains(value){
          if(this.root === null) return false;
